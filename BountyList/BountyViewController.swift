b3a000a6af9e102769e1ae58f4f08305e115e129
hojoon <ujuj0202@gmail.com>
2021-05-26 23:24:51 +0900

    tableview -> collectionView

--- BountyList/BountyViewController.swift
+++ BountyList/BountyViewController.swift
@@ -2,8 +2,9 @@
 
 import UIKit
 
-class BountyViewController: UIViewController,UITableViewDataSource,
-                            UITableViewDelegate{
+class BountyViewController: UIViewController,UICollectionViewDelegate,
+                            UICollectionViewDataSource,UICollectionViewDelegateFlowLayout{
+
     // refactorying mvvm
     // model
     // -- bountyInfo
@@ -23,25 +24,49 @@ class BountyViewController: UIViewController,UITableViewDataSource,
     override func viewDidLoad() {
         super.viewDidLoad() 
     }
-    //UITableViewDataSource
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return viewModel.numOfBountyInfoList
     }
     
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)as? ListCell else{ //downCasting
-            return UITableViewCell()
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "GridCell", for: indexPath)as? GridCell else {
+            return UICollectionViewCell()
         }
-        let bountyInfo = viewModel.bountyInfo(at: indexPath.row)
+        let bountyInfo = viewModel.bountyInfo(at: indexPath.item)
         cell.update(info: bountyInfo)
         return cell
     }
-    //delegate
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        print("--> \(indexPath.row)")
-        performSegue(withIdentifier: "showDetail", sender: indexPath.row)
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        print(indexPath.item)
+        performSegue(withIdentifier: "showDetail", sender: indexPath.item)
     }
-
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let itemSpacing:CGFloat = 10
+        let textAreaHeight:CGFloat = 65
+        let width:CGFloat = (collectionView.bounds.width-itemSpacing)/2
+        let height:CGFloat = width*10/7 + textAreaHeight
+        return CGSize(width: width, height: height)
+    }
+    //UITableViewDataSource
+//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        return viewModel.numOfBountyInfoList
+//    }
+//
+//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        guard let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)as? ListCell else{ //downCasting
+//            return UITableViewCell()
+//        }
+//        let bountyInfo = viewModel.bountyInfo(at: indexPath.row)
+//        cell.update(info: bountyInfo)
+//        return cell
+//    }
+//    //delegate
+//    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//        print("--> \(indexPath.row)")
+//        performSegue(withIdentifier: "showDetail", sender: indexPath.row)
+//    }
 }
 class ListCell:UITableViewCell{
     @IBOutlet weak var imgView: UIImageView!
@@ -76,5 +101,16 @@ class BountyViewModel {
     }
     func bountyInfo(at index:Int)->BountyInfo{
         return sortedList[index]
+    }
+}
+
+class GridCell:UICollectionViewCell{
+    @IBOutlet weak var imgView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var bountyLabel: UILabel!
+    func update(info:BountyInfo){
+        imgView.image = info.image
+        nameLabel.text = info.name
+        bountyLabel.text = "\(info.bounty)"
     }
 }
